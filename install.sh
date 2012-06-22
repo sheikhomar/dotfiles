5c583b291cceab24ed341fad7e80c0d4ef5ac5e5
Omar Ali Sheikh <sheikhomar@gmail.com>
2012-06-22 10:18:35 +0200

    Simplify install script.

--- install.sh
+++ install.sh
@@ -3,33 +3,22 @@
 BACKUP_DIR="/tmp/dots$RANDOM"
 LOG_FILE=$BACKUP_DIR/log
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+declare -a files=(bashrc zshrc gitconfig vimrc Xmodmap gemrc vim zsh)
 
 echo Backing up files to $BACKUP_DIR
 mkdir $BACKUP_DIR
-cp $HOME/.bashrc $BACKUP_DIR/bashrc &>> $LOG_FILE
-cp $HOME/.zshrc $BACKUP_DIR/zshrc &>> $LOG_FILE
-cp $HOME/.gitconfig $BACKUP_DIR/gitconfig &>> $LOG_FILE
-cp $HOME/.vimrc $BACKUP_DIR/vimrc &>> $LOG_FILE
-cp $HOME/.Xmodmap $BACKUP_DIR/Xmodmap &>> $LOG_FILE
-cp -Lr $HOME/.vim $BACKUP_DIR/vim &>> $LOG_FILE
-cp -Lr $HOME/.zsh $BACKUP_DIR/zsh &>> $LOG_FILE
+for file in ${files[@]}; do
+    cp -Lr $HOME/.${file} $BACKUP_DIR/${file} &>> $LOG_FILE
+done
 
-echo Removing old dot files
-rm -f $HOME/.bashrc &>> $LOG_FILE
-rm -f $HOME/.zshrc &>> $LOG_FILE
-rm -f $HOME/.gitconfig &>> $LOG_FILE
-rm -f $HOME/.vimrc &>> $LOG_FILE
-rm -r $HOME/.Xmodmap &>> $LOG_FILE
-rm -rf $HOME/.vim &>> $LOG_FILE
-rm -rf $HOME/.zsh &>> $LOG_FILE
+echo Removing old files
+for file in ${files[@]}; do
+    rm -rf $HOME/.${file} &>> $LOG_FILE
+done
 
-echo Installing new dot files
-ln -nfs $SRC_DIR/bashrc $HOME/.bashrc
-ln -nfs $SRC_DIR/zshrc $HOME/.zshrc
-ln -nfs $SRC_DIR/gitconfig $HOME/.gitconfig
-ln -nfs $SRC_DIR/vimrc $HOME/.vimrc
-ln -nfs $SRC_DIR/Xmodmap $HOME/.Xmodmap
-ln -nfs $SRC_DIR/vim $HOME/.vim
-ln -nfs $SRC_DIR/zsh $HOME/.zsh
+echo Installing new files
+for file in ${files[@]}; do
+    ln -nfs $SRC_DIR/${file} $HOME/.${file}
+done
 
 echo Done. Log file: $LOG_FILE
